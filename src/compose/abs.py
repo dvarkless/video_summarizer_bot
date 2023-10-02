f17669f29564def9e1c3b5222aaf76b4720cfad5
dvarkless <dvarkless@gmail.com>
2023-10-02 19:46:53 +0300

    Add footer and backindex

--- src/compose/abs.py
+++ src/compose/abs.py
@@ -2,34 +2,41 @@ from pathlib import Path
 
 
 class Document:
-    def __init__(self, file_dir, file_name) -> None:
+    def __init__(self, file_dir: str | Path, file_name: str | Path, footer_text: str) -> None:
         self.file_dir = Path(file_dir)
         self.file_name = file_name
+        self.footer_text = footer_text
         self.text = ""
 
-    def plain(self, in_text: str):
+    def plain(self, in_text: str) -> None:
         raise NotImplementedError("Not implemented")
 
-    def write_file(self):
+    def write_file(self) -> None:
         raise NotImplementedError("Not implemented")
 
-    def h1(self, title: str):
+    def h1(self, title: str) -> None:
         raise NotImplementedError("Not implemented")
 
-    def h2(self, title: str):
+    def h2(self, title: str) -> None:
         raise NotImplementedError("Not implemented")
 
-    def h3(self, title: str):
+    def h3(self, title: str) -> None:
         raise NotImplementedError("Not implemented")
 
-    def index(self, chapters: list[str]):
+    def index(self, chapters: list[str], indexname: str = 'Index') -> None:
         raise NotImplementedError("Not implemented")
 
-    def citate(self, citate: str):
+    def backindex(self) -> None:
         raise NotImplementedError("Not implemented")
 
-    def sep(self):
+    def citate(self, citate: str) -> None:
         raise NotImplementedError("Not implemented")
 
-    def embedding(self, link: str):
+    def sep(self) -> None:
+        raise NotImplementedError("Not implemented")
+
+    def embedding(self, link: str) -> None:
+        raise NotImplementedError("Not implemented")
+
+    def footer(self) -> None:
         raise NotImplementedError("Not implemented")
