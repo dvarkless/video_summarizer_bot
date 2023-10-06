102e8cbaf1ae396eaa16a5acd72fc62a1b7f84bd
dvarkless <dvarkless@gmail.com>
2023-10-06 15:05:23 +0300

    Add bot command list, rename routers

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,15 +1,25 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, types
 
-import src.bot.settings as settings_router
-import src.bot.action_handlers as action_router
-
+from src.bot.action_handlers import router as action_router
+from src.bot.bot_locale import BotReply
+from src.bot.settings import router as settings_router
 from src.config import Config
-
 from src.setup_handler import get_handler
+
+
+async def set_default_commands(dp):
+    replicas = BotReply().replicas['English']
+    bot_commands = []
+    for key, val in replicas:
+        if key not in ('language', 'general', 'errors', 'welcome'):
+            cmd_desc = val['description']
+            bot_commands.append(
+                types.BotCommand(command=key, description=cmd_desc))
+
+    await dp.bot.set_my_commands(bot_commands)
 
 
 async def main() -> None:
@@ -21,10 +31,10 @@ async def main() -> None:
     logger = logging.getLogger(__name__)
     logger.addHandler(get_handler())
 
-    dp.include_routers(settings_router.router, action_router.router)
+    dp.include_routers(settings_router, action_router)
     await bot.delete_webhook(drop_pending_updates=True)
     print("bot activated")
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, on_startup=set_default_commands)
 
 
 if __name__ == "__main__":
