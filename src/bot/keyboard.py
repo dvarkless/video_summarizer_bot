d3efbadb7f6983b0ad6e1c00e8b57a2cc67b951f
dvarkless <dvarkless@gmail.com>
2023-10-11 19:04:21 +0300

    Change input argument from dict to list

--- src/bot/keyboard.py
+++ src/bot/keyboard.py
@@ -2,13 +2,12 @@ from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
-def keyboard(butn_dict: dict, placeholder: str | None = None) -> ReplyKeyboardMarkup:
+def keyboard(butn_list: list, placeholder: str | None = None) -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    butn_dict = butn_dict.copy()
-    buttons = sorted(butn_dict.items(), key=lambda x: x[0])
+    buttons = sorted(butn_list)
     num_buttons = min(len(buttons), 4)
-    for _, val in buttons:
-        kb.button(text=val)
+    for btn in buttons:
+        kb.button(text=btn)
     kb.adjust(num_buttons)
     return kb.as_markup(resize_keyboard=True,
                         input_field_placeholder=placeholder)
