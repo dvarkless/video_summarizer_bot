a1407276479b7f9803754a1c4a4ff5df8ba5b8fb
dvarkless <dvarkless@gmail.com>
2023-10-17 14:24:09 +0300

    Fix faster whisper inference error by exporting cuda paths

--- start_bot.sh
+++ start_bot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 source venv/bin/activate
+export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 export PYTHONPATH=$(pwd)
 export TELEGRAM_SUMMARY_BOT_CONFIG_ROOT=$(pwd)
 python src/bot/bot.py
