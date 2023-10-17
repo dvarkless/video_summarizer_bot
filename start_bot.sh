4043d13ce93239b6998f0818a7d70272045a9496
dvarkless <dvarkless@gmail.com>
2023-10-17 18:58:18 +0300

    Fix llamacpp installer script, fix libs in requirements.txt, check for mongodb system

--- start_bot.sh
+++ start_bot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#
+
+MONGO_ACTIVE=$(systemctl is-active mongodb.service)
+if [[ $MONGO_ACTIVE  != "active" ]]; then
+    echo "Please grant sudo access to enable mongodb service"
+    systemctl start mongodb.service
+fi
 source venv/bin/activate
 export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 export PYTHONPATH=$(pwd)
