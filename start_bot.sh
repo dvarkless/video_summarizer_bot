fe13e0b4f86243d5a9cceef19005985a0c16a04c
dvarkless <dvarkless@gmail.com>
2023-10-20 20:19:29 +0300

    Add checking for local DB

--- start_bot.sh
+++ start_bot.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-MONGO_ACTIVE=$(systemctl is-active mongodb.service)
-if [[ $MONGO_ACTIVE  != "active" ]]; then
-    echo "Please grant sudo access to enable mongodb service"
-    systemctl start mongodb.service
+MONGO_ADDR=$(cat ./configs/secrets.yaml | grep "mongodb_url:")
+if [[ $MONGO_ADDR != *"mongodb://127.0.0.1"* ]]; then
+    MONGO_LOCAL=true
+elif [[ $MONGO_ADDR != *"mongodb://localhost"* ]]; then
+    MONGO_LOCAL=true
+else
+    MONGO_LOCAL=false
 fi
+echo $MONGO_LOCAL
+if [[ $MONGO_LOCAL==true ]]; then
+    MONGO_ACTIVE=$(systemctl is-active mongodb.service)
+    if [[ $MONGO_ACTIVE  != "active" ]]; then
+        echo "Please grant sudo access to enable mongodb service"
+        systemctl start mongodb.service
+    fi
+fi
+
 source venv/bin/activate
 export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 export PYTHONPATH=$(pwd)
