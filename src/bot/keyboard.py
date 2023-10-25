a90706f05b9a3f8d2291bd84bbe60c2e9d9e2ecb
dvarkless <dvarkless@gmail.com>
2023-10-25 20:41:09 +0300

    Add rows and columns

--- src/bot/keyboard.py
+++ src/bot/keyboard.py
@@ -1,13 +1,14 @@
-from aiogram.types import ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 
 def keyboard(butn_list: list, placeholder: str | None = None) -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     buttons = sorted(butn_list)
-    num_buttons = min(len(buttons), 4)
+    rows = min(len(buttons), 4)
+    columns = len(buttons) % 4
     for btn in buttons:
         kb.button(text=btn)
-    kb.adjust(num_buttons)
+    kb.adjust(columns, rows)
     return kb.as_markup(resize_keyboard=True,
                         input_field_placeholder=placeholder)
