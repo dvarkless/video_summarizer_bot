99a1f7525bfa685af8429e4e4dc9202046b10072
dvarkless <dvarkless@gmail.com>
2023-10-26 15:55:46 +0300

    Add cloud whisper

--- src/process/model.py
+++ src/process/model.py
@@ -4,10 +4,12 @@ from pathlib import Path
 from typing import Tuple, Union
 
 import numpy as np
+import openai
 from faster_whisper import WhisperModel
 from langchain.embeddings import LlamaCppEmbeddings, OpenAIEmbeddings
 from langchain.embeddings.spacy_embeddings import SpacyEmbeddings
 from langchain.llms import LlamaCpp, OpenAI
+from pydub import AudioSegment
 
 from src.setup_handler import get_handler
 
@@ -57,6 +59,68 @@ class WhisperInference:
 
     def __del__(self):
         del self.model
+
+
+class WhisperCloud:
+    def __init__(
+        self,
+        model_name,
+        model_dir,
+        is_translate,
+        **whisper_params
+    ):
+        self.model_name = model_name
+        self.model_dir = model_dir
+        self.is_translate = is_translate
+        self.whisper_params = whisper_params
+
+    def transcribe(self,
+                   audio: str,
+                   language=None,
+                   **to_whisper_transcribe,
+                   ):
+        logger.info('Call: WhisperInference.transcribe')
+        start_time = time.time()
+        clip_paths = self._get_clips(audio)
+        result = self._process_list(clip_paths, **to_whisper_transcribe)
+
+        self.elapsed_time = time.time() - start_time
+        return result
+
+    def _process_list(self,
+                      audio_paths: list[str],
+                      **to_whisper_transcribe,
+                      ):
+        texts = []
+        for path in audio_paths:
+            if self.is_translate:
+                transcript = openai.Audio.translate(
+                    model=self.model_name, file=path, response_format="text",
+                    **to_whisper_transcribe)
+            else:
+                transcript = openai.Audio.transcribe(
+                    model=self.model_name, file=path, response_format="text",
+                    **to_whisper_transcribe)
+
+            texts.append(transcript)
+        return "\n".join(texts)
+
+    def _get_clips(self, audio: str) -> list[str]:
+        sound = AudioSegment.from_file(audio)
+        max_ms = len(sound)
+        step_ms = 10 * 60 * 1000
+        if step_ms >= max_ms:
+            return [audio]
+
+        out_paths = []
+        for i in range(max_ms//step_ms):
+            name = f"temp/temp_clip_{i}.mp3"
+            ms_upper = min((i+1) * step_ms, max_ms)
+            ms_lower = i * step_ms
+            clip = sound[ms_lower:ms_upper]
+            clip.export(name, format="mp3")
+            out_paths.append(name)
+        return out_paths
 
 
 class ConfigureModel:
