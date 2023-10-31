7be9cd223076eb4c5aa99b87ee55ecc43aae578c
dvarkless <dvarkless@gmail.com>
2023-10-31 15:54:33 +0300

    Edit logging module

--- src/setup_handler.py
+++ src/setup_handler.py
@@ -5,7 +5,7 @@ import logging.handlers
 def get_handler():
     handler = logging.handlers.RotatingFileHandler(
         'log.log', maxBytes=3*1024*1024, backupCount=3)
-    handler.setLevel(logging.DEBUG)
+    # handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
         "%(asctime)s in %(name)s: %(levelname)s MESSAGE:'%(message)s")
